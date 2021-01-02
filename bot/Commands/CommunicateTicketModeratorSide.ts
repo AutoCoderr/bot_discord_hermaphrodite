c311a515211fa41ca250929d715af0187f5e7b80
Juju <julienbouvet78@hotmail.com>
2021-01-02 19:31:23 +0100

    Switch the user server, when a moderator of a different server answer in the ticket

--- bot/Commands/CommunicateTicketModeratorSide.ts
+++ bot/Commands/CommunicateTicketModeratorSide.ts
@@ -10,21 +10,30 @@ export class CommunicateTicketModeratorSide extends Command {
     }
 
     static async action(message, bot) {
-        const userCommunication: ITicketCommunication = await TicketCommunication.findOne({ticketChannelId: message.channel.id});
-        if (userCommunication == null) return false;
+        const usedCommunication: ITicketCommunication = await TicketCommunication.findOne({ticketChannelId: message.channel.id});
+        if (usedCommunication == null) return false;
 
-        const userToWrite = getUserFromCache(userCommunication.customerId,bot);
+        const userToWrite = getUserFromCache(usedCommunication.customerId,bot);
         if (userToWrite == null) {
             message.channel.send("L'utilisateur auteur de ce ticket n'est pas présent dans le cache et ne peux donc pas être contacté");
             return false;
         }
 
         const currentTime = new Date();
-        if (currentTime.getTime() - userCommunication.lastUse > 5 * 60 * 1000) {
+        if (currentTime.getTime() - usedCommunication.lastUse > 5 * 60 * 1000 || !usedCommunication.usedByUser) {
             userToWrite.send("Un modérateur de '"+message.guild.name+"' vous répond :");
+            usedCommunication.usedByUser = true;
+            TicketCommunication.updateMany(
+                    {
+                        customerId: usedCommunication.customerId,
+                        serverId: { $ne: usedCommunication.serverId }
+                    },
+                    {
+                        $set: { usedByUser: false }
+                    }).then(_ => {});
         }
-        userCommunication.lastUse = currentTime.getTime(); // @ts-ignore
-        userCommunication.save();
+        usedCommunication.lastUse = currentTime.getTime(); // @ts-ignore
+        usedCommunication.save();
         userToWrite.send(message.content);
         message.channel.send("Votre message a été envoyé").then(sentMessage => {
             setTimeout(() => {
