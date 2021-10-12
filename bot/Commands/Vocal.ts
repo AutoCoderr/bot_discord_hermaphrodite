8a52f4b9eb69e23d997f3104de6e5cd523aac475
Julien Bouvet <julienbouvet78@hotmail.com>
2021-10-12 17:09:51 +0200

    Start Vocal action method

--- bot/Commands/Vocal.ts
+++ bot/Commands/Vocal.ts
@@ -1,6 +1,8 @@
 import Command from "../Classes/Command";
 import {GuildChannel, GuildMember, Message, MessageEmbed, VoiceChannel} from "discord.js";
 import config from "../config";
+import VocalSubscribe from "../Models/VocalSubscribe";
+import VocalConfig, {IVocalConfig} from "../Models/VocalConfig";
 
 export default class Vocal extends Command {
     static display = true;
@@ -27,16 +29,33 @@ export default class Vocal extends Command {
                 displayValidError: true,
                 type: "channels",
                 description: "Le ou les channels vocaux sur lesquels écouter",
-                valid: (channels: GuildChannel[]) => !channels.some(channel => channel.type != "GUILD_VOICE"),
+                valid: (channels: GuildChannel[]) => {
+                    const alreadySpecified = {};
+                    for (const channel of channels) {
+                        if (channel.type != "GUILD_VOICE" || alreadySpecified[channel.id])
+                            return false;
+                        alreadySpecified[channel.id] = true
+                    }
+                    return true;
+                },
                 errorMessage: () => ({
-                    name: "Channels non ou mal rentré",
-                    value: "Vous n'avez pas ou mal renseigné vos channels, ils ne peuvent être que des channels vocaux"
+                    name: "Channels mal rentré",
+                    value: "Vous avez mal renseigné vos channels. Ils ne peuvent être que des channels vocaux, et vous ne pouvez pas renseigner plus d'une fois le même channel"
                 })
             },
             users: {
                 required: (args) => args.help == undefined && args.action == "subscribe" && args.channels == undefined,
                 type: "users",
                 description: "Le ou les utilisateurs à écouter quand ils se connectent sur un vocal",
+                valid: (users: GuildMember[]) => {
+                    const alreadySpecified = {};
+                    for (const user of users) {
+                        if (alreadySpecified[user.id])
+                            return false;
+                        alreadySpecified[user.id] = true
+                    }
+                    return true;
+                },
                 errorMessage: (value, args) => (value == undefined && args.channels == undefined) ?
                         {
                             name: "Rentrez au moins l'un des deux",
@@ -62,7 +81,128 @@ export default class Vocal extends Command {
             return false;
         }
 
-        console.log(args);
+        if (this.message.guild == null) {
+            this.sendErrors( {
+                name: "Missing guild",
+                value: "We couldn't find the message guild"
+            });
+            return false;
+        }
+
+        const vocalConfig: IVocalConfig = await VocalConfig.findOne({serverId: this.message.guild.id, enabled: true});
+        if (vocalConfig == null) {
+            this.sendErrors( {
+                name: "Vocal désactivé",
+                value: "Vous ne pouvez pas executer cette commande car l'option d'abonnement vocal n'est pas activée sur ce serveur"
+            });
+            return false;
+        }
+        if (vocalConfig.listenerBlacklist.includes(this.message.author.id)) {
+            this.sendErrors( {
+                name: "Accès interdit",
+                value: "Vous n'avez visiblement pas le droit d'utiliser l'option d'abonnement vocal sur ce serveur"
+            });
+            return false;
+        }
+
+        switch (action) {
+            case 'subscribe':
+                const addeds: typeof VocalSubscribe[] = [];
+                const deleteds: typeof VocalSubscribe[] = [];
+                const forbiddens: { [id: string]: VoiceChannel|GuildMember|boolean } = {};
+
+                if (channels && users) {
+                    const alreadyAll: { [id: string]: VoiceChannel|GuildMember } = {};
+
+                    for (const channel of channels) {
+                        const allSubscribe = await VocalSubscribe.findOne({
+                            serverId: this.message.guild.id,
+                            channelId: channel.id,
+                            listenedId: 'all',
+                            listenerId: this.message.author.id
+                        });
+                        if (allSubscribe !== null) {
+                            alreadyAll[channel.id] = channel;
+                            continue;
+                        }
+
+                        if (forbiddens[channel.id] === undefined) {
+                            forbiddens[channel.id] = vocalConfig.channelBlacklist.includes(channel.id);
+                        }
+                        for (const user of users) {
+                            if (alreadyAll[user.id]) continue;
+                            const allSubscribe = await VocalSubscribe.findOne({
+                                serverId: this.message.guild.id,
+                                channelId: 'all',
+                                listenedId: user.id,
+                                listenerId: this.message.author.id
+                            });
+                            if (allSubscribe !== null) {
+                                alreadyAll[user.id] = user;
+                                continue;
+                            }
+
+                            if (forbiddens[user.id] === undefined) {
+                                forbiddens[user.id] = vocalConfig.listenableBlacklist.includes(user.id);
+                            }
+                            let subscribe: typeof VocalSubscribe = await VocalSubscribe.findOne({
+                                serverId: this.message.guild.id,
+                                channelId: channel.id,
+                                listenedId: user.id,
+                                listenerId: this.message.author.id
+                            });
+
+                            if (subscribe !== null) {
+                                subscribe.delete();
+                                deleteds.push({channel,user});
+                            } else  if (!forbiddens[user.id] && !forbiddens[channel.id]) {
+                                VocalSubscribe.create({
+                                    serverId: this.message.guild.id,
+                                    channelId: channel.id,
+                                    listenedId: user.id,
+                                    listenerId: this.message.author.id
+                                });
+                                addeds.push({channel,user});
+                            } else {
+                                if (forbiddens[user.id])
+                                    forbiddens[user.id] = user
+
+                                if (forbiddens[channel.id])
+                                    forbiddens[channel.id] = channel
+                            }
+                        }
+                    }
+                } else {
+                    for (const elem of (channels??users)) {
+                        forbiddens[elem.id] = channels ? vocalConfig.channelBlacklist.includes(elem.id) : vocalConfig.listenableBlacklist.includes(elem.id);
+
+                        const subscribe = await VocalSubscribe.findOne({
+                            serverId: this.message.guild.id,
+                            channelId: channels ? elem.id : 'all',
+                            listenedId: users ? elem.id : 'all',
+                            listenerId: this.message.author.id
+                        });
+                        if (subscribe !== null) {
+                            subscribe.delete();
+                            deleteds.push({channel: channels ? elem : 'all', user: users ? elem : 'all'});
+                        } else if (!forbiddens[elem.id]) {
+                            VocalSubscribe.deleteMany({
+                                serverId: this.message.guild.id,
+                                ...(channels ? {channelId: elem.id} : {listenedId: elem.id}),
+                                listenerId: this.message.author.id
+                            });
+                            VocalSubscribe.create({
+                                serverId: this.message.guild.id,
+                                channelId: channels ? elem.id : 'all',
+                                listenedId: users ? elem.id : 'all',
+                                listenerId: this.message.author.id
+                            })
+                            addeds.push({channel: channels ? elem : 'all', user: users ? elem : 'all'});
+                        } else
+                            forbiddens[elem.id] = elem;
+                    }
+                }
+        }
 
         return false;
     }
