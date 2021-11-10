d8c5195560633cc46f7832245cfdd89db982efd5
Julien BOUVET <julienbouvet78@hotmail.com>
2021-11-11 00:20:54 +0100

    Test Discord slash commands, to know how to implement these

--- bot/initSlashCommands.ts
+++ bot/initSlashCommands.ts
@@ -1,52 +1,132 @@
 import client from "./client";
 import {Interaction} from "discord.js";
-import {ApplicationCommandOptionType} from "discord-api-types/payloads/v9/_interactions/_applicationCommands/chatInput";
+import {ApplicationCommandOptionTypes} from "discord.js/typings/enums";
 
 export default async function initSlashCommands() {
     console.log("initSlashCommands");
 
+    for (const [,guild] of client.guilds.cache) {
+        console.log('Create commandes for ' + guild.name + ' server');
 
-    const commands = client.application?.commands;
-    console.log({commands});
-    console.log(commands?.cache.map(command => command.name));
-    let res
-    res = await commands?.create({
-        name: 'add',
-        description: 'Make a ping'
-    });
-    console.log({ping: res});
-    res = await commands?.create({
-        name: 'calcul',
-        description: 'calculer',
-        options: [
-            {
-                type: ApplicationCommandOptionType.Number,
-                name: 'num1',
-                description: "Premier nombre",
-                required: true
-            },
-            {
-                type: ApplicationCommandOptionType.Number,
-                name: 'num2',
-                description: "Deuxième nombre",
-                required: true
-            },
-            {
-                type: ApplicationCommandOptionType.Number,
-                name: 'truc',
-                description: "machin",
-                required: true
-            }]
-    });
-    console.log({calcul: res});
-    console.log("created");
-    console.log(commands?.cache.map(command => command.name));
+        try {
+            const commands = guild.commands;
 
-    if (true) {
-        console.log("delete fucking wtf commands");
-        for (const [,command] of commands?.cache??[]) {
-            console.log("delete "+command.name);
-            await command.delete();
+            console.log({commands});
+            console.log(commands?.cache.map(command => command.name));
+            let res
+            res = await commands?.create({
+                name: 'ping',
+                description: 'Make a ping'
+            });
+            console.log({ping: res});
+            res = await commands?.create({
+                name: 'add',
+                description: 'Additionner',
+                options: [
+                    {
+                        type: ApplicationCommandOptionTypes.NUMBER,
+                        name: 'num1',
+                        description: "Premier nombre",
+                        required: true
+                    },
+                    {
+                        type: ApplicationCommandOptionTypes.NUMBER,
+                        name: 'num2',
+                        description: "Deuxième nombre",
+                        required: true
+                    }]
+            });
+            console.log({add: res});
+
+            res = await commands?.create({
+                name: 'test',
+                description: 'Test pour les sous commandes',
+                options: [
+                    {
+                        name: 'get',
+                        description: 'Get',
+                        type: ApplicationCommandOptionTypes.SUB_COMMAND,
+                        options: [
+                            {
+                                required: true,
+                                name: 'user',
+                                description: 'wesh',
+                                type: ApplicationCommandOptionTypes.USER
+                            },
+                            {
+                                required: false,
+                                name: 'emote',
+                                description: 'Une emote',
+                                type: ApplicationCommandOptionTypes.STRING
+                            }
+                        ]
+                    },
+                    {
+                        name: 'edit',
+                        description: 'Edit',
+                        type: ApplicationCommandOptionTypes.SUB_COMMAND,
+                        options: [
+                            {
+                                required: true,
+                                name: 'user',
+                                description: 'wesh',
+                                type: ApplicationCommandOptionTypes.USER
+                            }
+                        ]
+                    },
+                    {
+                        name: 'show',
+                        description: 'show',
+                        type: ApplicationCommandOptionTypes.SUB_COMMAND,
+                        options: [
+                            {
+                                name: 'detailed',
+                                description: "bip boup bip",
+                                type: ApplicationCommandOptionTypes.BOOLEAN,
+                                required: false,
+                            }
+                        ]
+                    },
+                    {
+                        name: 'list',
+                        description: 'List',
+                        type: ApplicationCommandOptionTypes.SUB_COMMAND_GROUP,
+                        options: [
+                            {
+                                name: 'fully',
+                                description: 'Totalement',
+                                type: ApplicationCommandOptionTypes.SUB_COMMAND
+                            }
+                        ]
+                    },
+                    /*{
+                        name: 'truc',
+                        description: 'truc',// @ts-ignore
+                        type: ApplicationCommandOptionTypes.SUB_COMMAND_GROUP,
+                        options: [
+                            {
+                                name: 'machin',
+                                description: 'machin',// @ts-ignore
+                                type: ApplicationCommandOptionTypes.SUB_COMMAND_GROUP,
+                                options: [
+                                    {
+                                        name: 'bidule',
+                                        description: 'bidule',// @ts-ignore
+                                        type: ApplicationCommandOptionTypes.SUB_COMMAND
+                                    }
+                                ]
+                            }
+                        ]
+                    }*/
+                ]
+            })
+            console.log({test: res})
+
+            console.log("created");
+            console.log(commands?.cache.map(command => command.name));
+        } catch (e) {
+            console.error(e)
+            console.log("Command can't be created on the '" + guild.name + "' server");
         }
     }
 }
@@ -66,9 +146,24 @@ export async function listenSlashCommands(interaction: Interaction) {
                 content: 'pong'
             });
             break;
-        case 'calcul':
+        case 'add':
             await interaction.reply({
                 content: options.getNumber('num1')+' + '+options.getNumber('num2')+' = '+((options.getNumber('num1')??0) + (options.getNumber('num2')??0)),
+                ephemeral: true
+            });
+            break;
+        case 'test':
+            const user = options.getUser('user');
+            const subCommand = options.getSubcommand();
+            let subCommandGroup: null|string = null;
+            try {
+                subCommandGroup = options.getSubcommandGroup();
+            } catch (_) {}
+            const detailed = options.getBoolean('detailed');
+            const emote = options.getString('emote');
+            console.log({user,subCommand,subCommandGroup,detailed,emote});
+            await interaction.reply({
+                content: "CECI EST UNE COMMANDE TEST",
                 ephemeral: true
             })
     }
