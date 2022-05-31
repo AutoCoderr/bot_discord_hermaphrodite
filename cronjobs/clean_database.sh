95c8c17f0a3f8f1fa7f375a362af6b4cfd651930
Julien Bouvet <julienbouvet78@hotmail.com>
2022-05-31 14:50:57 +0200

    Factorize script in many files, and improve script

--- cronjobs/clean_database.sh
+++ cronjobs/clean_database.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-echo -e "\n$(date +"%Y-%m-%d %T") :\n$(/usr/local/bin/node /bot/scripts/cleanDatabase.js)" >> /script_logs/clean_database.log 2>&1
+echo -e "\n$(date +"%Y-%m-%d %T") :\n$(/usr/local/bin/node /bot/scripts/cleanDatabase/cleanDatabase.js)" >> /script_logs/clean_database.log 2>&1
